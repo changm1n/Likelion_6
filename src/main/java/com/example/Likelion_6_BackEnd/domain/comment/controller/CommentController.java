d3bc698cf66273c2f6f2951a75518fbcaa60d626
changm1n <leo4109@gmail.com>
2024-07-31 00:50:24 +0900

    feat : 후기 조회 삭제 구현

--- src/main/java/com/example/Likelion_6_BackEnd/domain/comment/controller/CommentController.java
+++ src/main/java/com/example/Likelion_6_BackEnd/domain/comment/controller/CommentController.java
@@ -7,12 +7,10 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -26,6 +24,18 @@ public class CommentController {
     public ResponseEntity<?> create(@RequestBody CommentRequestDTO commentRequestDTO, @PathVariable Long recipeId, HttpSession httpSession) throws IOException {
         String userId = (String) httpSession.getAttribute("loginId");
         CommentResponseDTO result = commentService.create(commentRequestDTO,userId,recipeId);
+        return ResponseEntity.ok().body(result);
+    }
+    // 후기 조회
+    @GetMapping("/board/{recipeId}/comment")
+    public ResponseEntity<?> commentList(@PathVariable Long recipeId){
+        List<CommentResponseDTO> result = commentService.commentList(recipeId);
+        return ResponseEntity.ok().body(result);
+    }
+    // 후기 삭제
+    @PostMapping("/board/delete/{recipeId}")
+    public ResponseEntity<?> delete(@PathVariable Long recipeId){
+        String result = commentService.delete(recipeId);
         return ResponseEntity.ok().body(result);
     }
 }
